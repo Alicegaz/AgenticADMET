58992de4adc92343f2bdca11c6320bc9ec4f5b31
Alicegaz <gazizullina2010@yandex.ru>
2025-02-24 00:52:04 +0000

    fix: explicit super call for _prepare_inputs

--- train.py
+++ train.py
@@ -186,7 +186,7 @@ class GRPOTrainer2(GRPOTrainer):
         prompt_inputs = self.processing_class(
             prompts_text, return_tensors="pt", padding=True, padding_side="left", add_special_tokens=False
         )
-        prompt_inputs = Trainer._prepare_inputs(self, prompt_inputs)
+        prompt_inputs = Trainer._prepare_inputs(self, prompt_inputs, paddin_side="left")
         prompt_ids, prompt_mask = prompt_inputs["input_ids"], prompt_inputs["attention_mask"]
 
         if self.max_prompt_length is not None:
